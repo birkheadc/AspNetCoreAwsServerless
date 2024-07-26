60fe636b80cd2215c16dfc748f761877ec94a3d7
Colby Birkhead <birkheadc@gmail.com>
2024-07-26 22:44:06 +0900

    decorate Book entity for use in dynamodb

--- src/AspNetCoreAwsServerless/Entities/Books/Book.cs
+++ src/AspNetCoreAwsServerless/Entities/Books/Book.cs
@@ -1,9 +1,12 @@
+using Amazon.DynamoDBv2.DataModel;
 using AspNetCoreAwsServerless.Utils.Id;
 
 namespace AspNetCoreAwsServerless.Entities.Books;
 
+[DynamoDBTable("Books")]
 public class Book
 {
+  [DynamoDBHashKey]
   public required Id<Book> Id { get; init; }
   public required string Title { get; init; }
   public required string Author { get; init; }
