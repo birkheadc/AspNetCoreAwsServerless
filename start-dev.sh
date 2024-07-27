227f1859f85444a5f306584207acb65bc00f55f5
Colby Birkhead <birkheadc@gmail.com>
2024-07-27 19:56:12 +0900

    add restart_on_rude_edit flag to start-dev

--- start-dev.sh
+++ start-dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Starting API..."
-ASPNETCORE_ENVIRONMENT=Development dotnet watch --project src/AspNetCoreAwsServerless/AspNetCoreAwsServerless.csproj
+ASPNETCORE_ENVIRONMENT=Development DOTNET_WATCH_RESTART_ON_RUDE_EDIT=1 dotnet watch --project src/AspNetCoreAwsServerless/AspNetCoreAwsServerless.csproj
