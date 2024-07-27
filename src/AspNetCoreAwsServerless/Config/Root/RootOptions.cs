4830169d084bf98af955f0cec3a7785746c8821d
Colby Birkhead <birkheadc@gmail.com>
2024-07-27 20:00:25 +0900

    create RootOptions

---
+++ src/AspNetCoreAwsServerless/Config/Root/RootOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreAwsServerless.Config.Root
+{
+  public class RootOptions
+  {
+    public string Greeting { get; set; } = "";
+  }
+}
