28f6707a02a598fae6c5d90edba7fc925b57c938
Colby Birkhead <birkheadc@gmail.com>
2024-07-31 19:21:58 +0900

    create staging branch

--- test/AspNetCoreAwsServerless.Tests/Unit/Controllers/Root/RootControllerTests.cs
+++ test/AspNetCoreAwsServerless.Tests/Unit/Controllers/Root/RootControllerTests.cs
@@ -5,26 +5,31 @@ using Microsoft.Extensions.Options;
 using Moq.AutoMock;
 using Xunit;
 
-namespace AspNetCoreAwsServerless.Tests.Unit.Controllers.Root
+namespace AspNetCoreAwsServerless.Tests.Unit.Controllers.Root;
+
+public class RootControllerTests
 {
-  public class RootControllerTests
+  private readonly RootOptions options = new() { Greeting = "Test Greeting!" };
+
+  [Fact]
+  public async Task Get_ReturnsGreeting()
   {
-    private readonly RootOptions options = new() { Greeting = "Test Greeting!" };
+    string expected = options.Greeting;
 
-    [Fact]
-    public async void Get_ReturnsGreeting()
-    {
-      string expected = options.Greeting;
+    AutoMocker mocker = new();
 
-      AutoMocker mocker = new();
+    mocker.GetMock<IOptions<RootOptions>>().SetupGet(mock => mock.Value).Returns(options);
 
-      mocker.GetMock<IOptions<RootOptions>>().SetupGet(mock => mock.Value).Returns(options);
+    RootController controller = mocker.CreateInstance<RootController>();
+    ActionResult<string> result = await controller.Get();
 
-      RootController controller = mocker.CreateInstance<RootController>();
-      ActionResult<string> result = await controller.Get();
+    OkObjectResult okObjectResult = Assert.IsType<OkObjectResult>(result.Result);
+    Assert.Equal(expected, okObjectResult.Value);
+  }
 
-      OkObjectResult okObjectResult = Assert.IsType<OkObjectResult>(result.Result);
-      Assert.Equal(expected, okObjectResult.Value);
-    }
+  [Fact]
+  public void Fails()
+  {
+    Assert.False(true);
   }
 }
