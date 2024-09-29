e99f94c3a1a6cc1696f9d60bb9ea81b470b6f068
Colby Birkhead <birkheadc@gmail.com>
2024-09-29 19:03:18 +0900

    add environment var to start script

--- start-dev.sh
+++ start-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 clear
 echo "Starting API..."
-ASPNETCORE_ENVIRONMENT=Development  ASPNETCORE_COGNITO_USER_POOL_ID="getthisvaluefromdevelopment" DOTNET_WATCH_RESTART_ON_RUDE_EDIT=1 dotnet watch --project src/AspNetCoreAwsServerless/AspNetCoreAwsServerless.csproj
+ASPNETCORE_ENVIRONMENT=Development  ASPNETCORE_COGNITO_USER_POOL_ID="ap-southeast-2_p9VUYZmuz" DOTNET_WATCH_RESTART_ON_RUDE_EDIT=1 dotnet watch --project src/AspNetCoreAwsServerless/AspNetCoreAwsServerless.csproj
