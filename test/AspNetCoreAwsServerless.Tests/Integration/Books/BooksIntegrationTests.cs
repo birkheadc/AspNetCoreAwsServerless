d3141bf99711077668b4ebc45abc967382104b56
Colby Birkhead <birkheadc@gmail.com>
2024-10-16 21:21:36 +0900

    comment out integration tests for now

--- test/AspNetCoreAwsServerless.Tests/Integration/Books/BooksIntegrationTests.cs
+++ test/AspNetCoreAwsServerless.Tests/Integration/Books/BooksIntegrationTests.cs
@@ -1,16 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Xunit;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using Xunit;
 
-namespace AspNetCoreAwsServerless.Tests.Integration.Books;
+// namespace AspNetCoreAwsServerless.Tests.Integration.Books;
 
-public class BooksIntegrationTests
-{
-  [Fact]
-  public void Test1()
-  {
-    Assert.True(true);
-  }
-}
+// public class BooksIntegrationTests
+// {
+//   [Fact]
+//   public void Test1()
+//   {
+//     Assert.True(true);
+//   }
+// }
