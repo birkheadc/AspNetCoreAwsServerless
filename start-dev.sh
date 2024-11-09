61df349dbddad4ff9565bb0dd30f31294e55ee5c
Colby Birkhead <birkheadc@gmail.com>
2024-11-09 11:56:21 +0900

    remove unneeded appsettings; update start-dev command

--- start-dev.sh
+++ start-dev.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 clear
 echo "Starting API..."
-ASPNETCORE_ENVIRONMENT=Development  ASPNETCORE_COGNITO_USER_POOL_ID="ap-southeast-2_p9VUYZmuz" DOTNET_WATCH_RESTART_ON_RUDE_EDIT=1 dotnet watch --project src/AspNetCoreAwsServerless/AspNetCoreAwsServerless.csproj
+ASPNETCORE_ENVIRONMENT=Development \
+ASPNETCORE_COGNITO_CLIENT_ID="27efufoivmsq7uuhtdrs1rp6af" \
+ASPNETCORE_COGNITO_URL="https://aspnetcoreserverless-development.auth.ap-southeast-2.amazoncognito.com" \
+DOTNET_WATCH_RESTART_ON_RUDE_EDIT=1 \
+dotnet watch --project src/AspNetCoreAwsServerless/AspNetCoreAwsServerless.csproj
